aba19ef959eacbc813ba7caabb97f7b42450bf40
Taiki Koyama <taikikoyama0701@gmail.com>
2018-10-11 15:00:11 +1000

    commit

---
+++ src/components/EventList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="event-list frame">
+        Event list
+    </div>
+</template>
+
+<script>
+export default {
+    name: "EventList"
+}
+</script>
+
+
+<style lang="scss" scoped>
+.event-list {
+
+}
+</style>
+
